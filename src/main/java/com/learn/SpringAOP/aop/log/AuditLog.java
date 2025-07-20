d6fddb40d84a2748bf6e17c036076b43dde9db90
ChanduRoyalA <chandua.royal@gmail.com>
2025-07-20 17:27:55 +0530

    Log Auth Req/Res

--- src/main/java/com/learn/SpringAOP/aop/log/AuditLog.java
+++ src/main/java/com/learn/SpringAOP/aop/log/AuditLog.java
@@ -2,6 +2,7 @@ package com.learn.SpringAOP.aop.log;
 
 
 import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
@@ -10,9 +11,11 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
 import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.context.request.ServletRequestAttributes;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseBodyAdvice;
 
 import java.nio.charset.StandardCharsets;
 import java.util.Base64;
+import java.util.Optional;
 
 @Component
 @Aspect
@@ -24,24 +27,52 @@ public class AuditLog {
 
     @Around("logAuth()")
     public Object LogMethod(ProceedingJoinPoint joinPoint) throws Throwable {
-        ServletRequestAttributes servletRequestAttributes = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
-        HttpServletRequest request = servletRequestAttributes.getRequest();
-        String authType = request.getAuthType();
-        String authHeader = request.getHeader("Authorization");
-        System.out.println("User Login Request");
-        System.out.println("Auth: ");
-        if (authHeader != null && authHeader.startsWith("Basic ")) {
-            String base64Credentials = authHeader.substring("Basic ".length());
-            byte[] credDecoded = Base64.getDecoder().decode(base64Credentials);
-            String credentials = new String(credDecoded, StandardCharsets.UTF_8);
-            String[] values = credentials.split(":", 2); // username:password
-            String usernameFromHeader = values[0];
-            System.out.println("    Username from Header: " + usernameFromHeader);
+        logAuthRequest();
+        Object res = null;
+        String resInfo;
+        try {
+            res = joinPoint.proceed();
+            UserDetails userDetails = ((UserDetails) res);
+            String username = userDetails.getUsername();
+            resInfo = "User (" + username + ") Logged In";
+        } catch (Exception e) {
+            resInfo = "Response: " + e.getMessage();
         }
-        Object res = joinPoint.proceed();
-        UserDetails userDetails = ((UserDetails) res);
-        String username = userDetails.getUsername();
-        System.out.println("User: " + username + " Logged in");
+        logAuthResponse(resInfo);
         return res;
+    }
+
+    private void logAuthResponse(String resInfo) {
+        ServletRequestAttributes servletRequestAttributes = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
+        if (servletRequestAttributes != null) {
+            HttpServletResponse response = servletRequestAttributes.getResponse();
+            System.out.println(resInfo);
+            return;
+        }
+        System.out.println(Optional.ofNullable(null));
+    }
+
+    private void logAuthRequest() {
+        ServletRequestAttributes servletRequestAttributes = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
+        if (servletRequestAttributes != null) {
+            HttpServletRequest request = servletRequestAttributes.getRequest();
+            String authHeader = request.getHeader("Authorization");
+            String reqUrl = (request.getRequestURL()).toString();
+            String userNameFromRequest = null;
+            if (authHeader != null && authHeader.startsWith("Basic ")) {
+                String base64Credentials = authHeader.substring("Basic ".length());
+                byte[] credDecoded = Base64.getDecoder().decode(base64Credentials);
+                String credentials = new String(credDecoded, StandardCharsets.UTF_8);
+                String[] values = credentials.split(":", 2); // username:password
+                userNameFromRequest = values[0];
+            }
+
+            String reqInfo = """
+                    User Login Request -> %s, User Name in the Request -> %s
+                    """.formatted(reqUrl, userNameFromRequest);
+
+            System.out.println(reqInfo);
+        }
+
     }
 }
--- src/main/java/com/learn/SpringAOP/aop/service/MyUserDetailsService.java
+++ src/main/java/com/learn/SpringAOP/aop/service/MyUserDetailsService.java
@@ -3,11 +3,17 @@ package com.learn.SpringAOP.aop.service;
 import com.learn.SpringAOP.aop.model.User;
 import com.learn.SpringAOP.aop.model.UserPrincipal;
 import com.learn.SpringAOP.aop.repo.UserRepo;
+import jakarta.servlet.http.HttpServletResponse;
+import org.apache.coyote.Response;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.net.http.HttpRequest;
 
 
 @Service
@@ -20,8 +26,7 @@ public class MyUserDetailsService implements UserDetailsService {
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         User user = userRepo.findByUserName(username);
         if (user == null) {
-            System.out.println("User Not Found");
-            throw new UsernameNotFoundException("User Not Found");
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
         }
         return new UserPrincipal(user);
     }
